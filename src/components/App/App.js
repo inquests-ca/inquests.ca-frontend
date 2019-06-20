7208feff4a8886da3a1c9dec5aef1be751f26832
Michael Eden <Mikey.Eden@gmail.com>
2019-06-20 12:19:41 -0400

    Add table of inquests to landing page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 
-const App = () => (
-  <div>
-    <header>
-      <h1>Welcome to inquests.ca!</h1>
-    </header>
-  </div>
-)
+import InquestTable from '../InquestTable';
 
-export default App;
+const useStyles = makeStyles(theme => ({
+  layout: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  }
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <Container maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.layout}>
+        <Typography variant="h1">Inquests.ca</Typography>
+        <InquestTable />
+      </div>
+    </Container>
+  );
+}
