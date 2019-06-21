fff0f6a6c576ded2f261b9dc41620a6aadcbc0cf
Michael Eden <Mikey.Eden@gmail.com>
2019-06-21 17:28:10 -0400

    Add router component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import ViewInquests from '../../scenes/ViewInquests';
 
 export default function App() {
   return (
-    <div>
+    <Router>
       <CssBaseline />
-      <ViewInquests />
-    </div>
+      <Route path="/" component={ViewInquests} />
+    </Router>
   );
 }
