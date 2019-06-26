472659f5607996565fe7d8dcaf9bfd5e65f5dff0
Michael Eden <Mikey.Eden@gmail.com>
2019-06-26 12:37:44 -0400

    Rewrite InquestTable component as functional component

--- src/scenes/ViewInquests/components/InquestTable/InquestTable.js
+++ src/scenes/ViewInquests/components/InquestTable/InquestTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -6,49 +6,45 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 
-// TODO: convert to functional component with React hooks.
-export default class InquestTable extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { inquests: [] };
-  }
+export default function InquestTable() {
+  const [inquests, setInquests] = useState(null);
 
-  componentDidMount() {
-    this.fetchInquests();
-  }
+  const fetchInquests = async () => {
+    const response = await fetch('/api/inquests');
+    let json;
+    try {
+      json = await response.json();
+    } catch (e) {
+      // TODO: log, and display error in client.
+      json = [];
+    }
+    setInquests(json);
+  };
 
-  render() {
-    return (
-      <Paper>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>Title</TableCell>
-              <TableCell>Description</TableCell>
-            </TableRow>
-          </TableHead>
+  useEffect(() => {
+    if (inquests === null) fetchInquests();
+  });
+
+  return (
+    <Paper>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Title</TableCell>
+            <TableCell>Description</TableCell>
+          </TableRow>
+        </TableHead>
+        {inquests && (
           <TableBody>
-            {this.state.inquests.map((inquest, i) => (
+            {inquests.map((inquest, i) => (
               <TableRow key={i}>
                 <TableCell>{inquest.title}</TableCell>
                 <TableCell>{inquest.description}</TableCell>
               </TableRow>
             ))}
           </TableBody>
-        </Table>
-      </Paper>
-    );
-  }
-
-  async fetchInquests() {
-    const response = await fetch('/api/inquests');
-    let inquests;
-    try {
-      inquests = await response.json();
-    } catch (e) {
-      // TODO: log, and display error in client.
-      inquests = [];
-    }
-    this.setState({ inquests: inquests });
-  }
+        )}
+      </Table>
+    </Paper>
+  );
 }
