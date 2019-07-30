a5c7b99dfa7afec1af70567ceb492f8a5dc1256c
Michael Eden <Mikey.Eden@gmail.com>
2019-07-30 15:18:49 -0400

    Create redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import { init as initFirebase } from './common/services/firebase';
 import App from './common/components/App';
+import configureStore from './configureStore';
 import * as serviceWorker from './serviceWorker';
 
 initFirebase();
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore({});
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
