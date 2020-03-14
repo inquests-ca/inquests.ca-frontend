8d070fd6e239cd46a2e3717a9644fce0d9ba88a4
Michael Eden <michael.eden100@gmail.com>
2020-03-14 14:17:00 -0400

    Create config object to store API invocation URL

--- src/common/services/fetchHelper/fetchHelper.js
+++ src/common/services/fetchHelper/fetchHelper.js
@@ -1,5 +1,9 @@
+// TODO: configure path variables to avoid the need for so many ..
+import config from '../../../config.prod.js';
+
 const defaultOptions = {
   credentials: 'omit'
 };
 
-export const fetchNoAuth = (url, options = {}) => fetch(url, { ...defaultOptions, ...options });
+export const fetchNoAuth = (url, options = {}) =>
+  fetch(config.api.invokeUrl + url, { ...defaultOptions, ...options });
---
+++ src/config.prod.js
@@ -0,0 +1,6 @@
+export default {
+  api: {
+    // TODO: get production invokeUrl
+    invokeUrl: 'http://127.0.0.1:9000'
+  }
+};
