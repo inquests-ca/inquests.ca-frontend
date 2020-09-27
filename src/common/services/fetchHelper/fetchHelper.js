c3cf806f50575e5939057264f02a5cd6511cc236
Michael Eden <michael.eden100@gmail.com>
2020-09-27 11:23:01 -0400

    Delete fetchHelper.js

--- src/common/services/fetchHelper/fetchHelper.js
+++
@@ -1,9 +0,0 @@
-// TODO: configure path variables to avoid the need for so many ..
-import config from '../../../config.prod.js';
-
-const defaultOptions = {
-  credentials: 'omit'
-};
-
-export const fetchNoAuth = (url, options = {}) =>
-  fetch(config.api.invokeUrl + url, { ...defaultOptions, ...options });
