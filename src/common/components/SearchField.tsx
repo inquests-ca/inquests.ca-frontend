be242d557630fb88f5379b81998d0cfe71154e20
Michael Eden <michael.eden100@gmail.com>
2020-11-23 14:21:26 -0500

    Fix SearchField to prevent duplicate searches

--- src/common/components/SearchField.tsx
+++ src/common/components/SearchField.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 interface SearchFieldProps {
@@ -11,15 +11,24 @@ interface SearchFieldProps {
 
 const SearchField = ({ label, name, onSearch, fullWidth, className }: SearchFieldProps) => {
   const [text, setText] = useState('');
+  const prevText = useRef('');
+
+  const handleSearch = (): void => {
+    // Only perform search if the text field has changed.
+    if (text !== prevText.current) {
+      onSearch(text);
+      prevText.current = text;
+    }
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void =>
     setText(event.currentTarget.value);
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>): void => {
-    if (event.key === 'Enter') onSearch(text);
+    if (event.key === 'Enter') handleSearch();
   };
 
-  const handleBlur = (): void => onSearch(text);
+  const handleBlur = (): void => handleSearch();
 
   return (
     <TextField
