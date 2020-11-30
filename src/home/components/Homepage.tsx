0925645b4b9eb144db9a69822ee40238a53518fd
Michael Eden <michael.eden100@gmail.com>
2020-11-30 11:31:47 -0500

    Replace homepage nav buttons with search components

--- src/home/components/Homepage.tsx
+++ src/home/components/Homepage.tsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom';
+
+import RadioButtons from 'common/components/RadioButtons';
+import SearchField from 'common/components/SearchField';
+import { AuthorityOrInquest, MenuItem } from 'common/types';
+import { stringifyQuery } from 'common/utils/request';
 
 const useStyles = makeStyles((theme) => ({
   layout: {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    margin: theme.spacing(2),
+    margin: theme.spacing(6),
   },
-  title: {
-    fontSize: '5rem',
-  },
+  title: { fontSize: '5rem' },
   subtitle: {
-    marginBottom: theme.spacing(4),
+    marginBottom: theme.spacing(8),
   },
-  button: {
-    margin: theme.spacing(2),
+  searchField: {
+    marginTop: theme.spacing(2),
+    maxWidth: '550px',
   },
 }));
 
 const Homepage = () => {
+  const history = useHistory();
+
+  const [searchType, setSearchType] = useState<AuthorityOrInquest>('authority');
+
+  const handleSearchTypeChange = (newSearchType: string) =>
+    setSearchType(newSearchType as AuthorityOrInquest);
+
+  const handleSearch = (text: string) => {
+    history.push(`/search${stringifyQuery({ type: searchType, text })}`);
+  };
+
   const classes = useStyles();
+
+  const searchOptions: MenuItem[] = [
+    {
+      value: 'authority',
+      label: (
+        <span>
+          <b>Authorities</b> &mdash; inquest-related case law.
+        </span>
+      ),
+    },
+    {
+      value: 'inquest',
+      label: (
+        <span>
+          <b>Inquests</b> &mdash; inquests including their verdicts and verdict explanations.
+        </span>
+      ),
+    },
+  ];
 
   return (
     <div className={classes.layout}>
@@ -33,24 +66,18 @@ const Homepage = () => {
       <Typography className={classes.subtitle} variant="h6" component="h2">
         A repository of Canadian inquest-related rulings, findings, and other authorities.
       </Typography>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="primary"
-        component={Link}
-        to="/authorities"
-      >
-        Search Authorities
-      </Button>
-      <Button
-        className={classes.button}
-        variant="contained"
-        color="primary"
-        component={Link}
-        to="/inquests"
-      >
-        Search Inquests
-      </Button>
+      <RadioButtons
+        items={searchOptions}
+        selectedValue={searchType}
+        onChange={handleSearchTypeChange}
+      ></RadioButtons>
+      <SearchField
+        onSearch={handleSearch}
+        label="Search"
+        name="search"
+        fullWidth
+        className={classes.searchField}
+      ></SearchField>
     </div>
   );
 };
