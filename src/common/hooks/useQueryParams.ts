3c8f23094e283b7b1add7431ca5f1cbf38fd0958
Michael Eden <michael.eden100@gmail.com>
2020-12-01 11:42:12 -0500

    Add JS Doc to useQueryParams

--- src/common/hooks/useQueryParams.ts
+++ src/common/hooks/useQueryParams.ts
@@ -3,6 +3,11 @@ import { useLocation } from 'react-router-dom';
 
 import { parseQuery } from 'common/utils/request';
 
+/**
+ * Returns an object representing current query parameters.
+ *
+ * @param schema Joi schema for validating query parameters.
+ */
 const useQueryParams = <T>(schema: joi.ObjectSchema<T>) => {
   const query = parseQuery(useLocation().search);
   const validation = schema.validate(query, { stripUnknown: true });
