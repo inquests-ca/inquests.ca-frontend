feb95a1635104260fdbb22c369aa079e075b499c
Michael Eden <michael.eden100@gmail.com>
2020-12-12 11:07:06 -0500

    Add loading prop to SingleSelect

--- src/common/components/SingleSelect.tsx
+++ src/common/components/SingleSelect.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import MuiMenuItem from '@material-ui/core/MenuItem';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { MenuItem, MenuItemValue } from 'common/types';
 
+const useStyles = makeStyles((_theme) => ({
+  loading: {
+    display: 'grid',
+    justifyItems: 'center',
+    alignItems: 'center',
+  },
+}));
+
 interface SingleSelectProps<T extends MenuItemValue> {
   items: MenuItem<T>[];
+  loading?: boolean;
   emptyItem?: boolean;
   selectedValue: T;
   onChange: (value: T) => void;
@@ -16,6 +27,7 @@ interface SingleSelectProps<T extends MenuItemValue> {
 
 export default function SingleSelect<T extends MenuItemValue>({
   items,
+  loading,
   emptyItem,
   selectedValue,
   onChange,
@@ -24,6 +36,19 @@ export default function SingleSelect<T extends MenuItemValue>({
 }: SingleSelectProps<T>) {
   const handleChange = (event: React.ChangeEvent<{ name?: string; value: unknown }>) =>
     onChange(event.target.value as T);
+
+  const classes = useStyles();
+
+  const renderMenuItems = () => {
+    if (items.length === 0) return null;
+    const itemElements = items.map((item, i) => (
+      <MuiMenuItem key={i} value={item.value}>
+        {item.label}
+      </MuiMenuItem>
+    ));
+    if (emptyItem) itemElements.unshift(<MuiMenuItem value="" />);
+    return itemElements;
+  };
 
   return (
     <FormControl className={className}>
@@ -39,12 +64,13 @@ export default function SingleSelect<T extends MenuItemValue>({
             : items.find((item) => item.value === value)?.label ?? ''
         }
       >
-        {emptyItem && <MuiMenuItem value="" />}
-        {items.map((item, i) => (
-          <MuiMenuItem key={i} value={item.value}>
-            {item.label}
-          </MuiMenuItem>
-        ))}
+        {loading ? (
+          <div className={classes.loading}>
+            <CircularProgress />
+          </div>
+        ) : (
+          renderMenuItems()
+        )}
       </Select>
     </FormControl>
   );
