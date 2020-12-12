2353869cdf0814dc55cfd2960313228a664e34da
Michael Eden <michael.eden100@gmail.com>
2020-12-12 12:13:16 -0500

    Add jurisdiction to BaseQuery

--- src/search/utils/api.ts
+++ src/search/utils/api.ts
@@ -14,6 +14,7 @@ export enum Sort {
 interface BaseQuery {
   text: string;
   keywords: string[];
+  jurisdiction: string;
   page: number;
   sort: Sort;
 }
@@ -21,6 +22,7 @@ interface BaseQuery {
 const baseQuerySchema = joi.object({
   text: joi.string(),
   keywords: joi.array(),
+  jurisdiction: joi.string(),
   page: joi.number().integer().positive().default(1),
   sort: joi.string().valid(...Object.values(Sort)),
 });
@@ -28,23 +30,33 @@ const baseQuerySchema = joi.object({
 const defaultBaseQuery = (): BaseQuery => ({
   text: '',
   keywords: [],
+  jurisdiction: '',
   page: 1,
   sort: Sort.Relevant,
 });
 
-// TODO: Do not show page query param in client when page=1.
 export type AuthorityQuery = BaseQuery;
-export const fetchAuthorities = ({ text, keywords, page, sort }: AuthorityQuery) => {
-  const query = { text, keywords, sort, limit: PAGINATION, offset: (page - 1) * PAGINATION };
-  return fetchJson<DataWithCount<Authority[]>>('/authorities', query);
+export const fetchAuthorities = (query: AuthorityQuery) => {
+  const apiQuery = {
+    ...query,
+    page: undefined, // Remove page query parameter (effectively replaced by offset).
+    limit: PAGINATION,
+    offset: (query.page - 1) * PAGINATION,
+  };
+  return fetchJson<DataWithCount<Authority[]>>('/authorities', apiQuery);
 };
 export const defaultAuthorityQuery = (): AuthorityQuery => defaultBaseQuery();
 export const authorityQuerySchema = baseQuerySchema;
 
 export type InquestQuery = BaseQuery;
-export const fetchInquests = ({ text, keywords, page, sort }: InquestQuery) => {
-  const query = { text, keywords, sort, limit: PAGINATION, offset: (page - 1) * PAGINATION };
-  return fetchJson<DataWithCount<Inquest[]>>('/inquests', query);
+export const fetchInquests = (query: InquestQuery) => {
+  const apiQuery = {
+    ...query,
+    page: undefined, // Remove page query parameter (effectively replaced by offset).
+    limit: PAGINATION,
+    offset: (query.page - 1) * PAGINATION,
+  };
+  return fetchJson<DataWithCount<Inquest[]>>('/inquests', apiQuery);
 };
 export const defaultInquestQuery = (): InquestQuery => defaultBaseQuery();
 export const inquestQuerySchema = baseQuerySchema;
