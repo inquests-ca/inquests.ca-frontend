d5c4019dc7f0195f1d955a351bbbd2be8b370d40
Michael Eden <michael.eden100@gmail.com>
2020-12-21 17:18:31 -0500

    Add Box component

---
+++ src/common/components/Box.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+const useStyles = makeStyles((theme) => ({
+  box: {
+    border: `1px solid ${theme.palette.divider}`,
+    borderRadius: theme.shape.borderRadius,
+  },
+  loading: {
+    display: 'grid',
+    justifyItems: 'center',
+    alignItems: 'center',
+  },
+}));
+
+interface BoxProps {
+  children: React.ReactNode;
+  label?: string;
+  loading?: boolean;
+  className?: string;
+}
+
+const Box = ({ children, label, loading, className }: BoxProps) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      {label && (
+        <Typography color="textSecondary" component="span" variant="body2">
+          {label}
+        </Typography>
+      )}
+      {loading ? (
+        <div className={clsx(classes.loading, classes.box)}>
+          <CircularProgress />
+        </div>
+      ) : (
+        <div className={clsx(className, classes.box)}>{children}</div>
+      )}
+    </div>
+  );
+};
+
+export default Box;
