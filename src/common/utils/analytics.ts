0f2887d3515a7c1f97ff0ac54638f9a744ea7ac6
Michael Eden <michael.eden100@gmail.com>
2020-12-28 12:00:40 -0500

    Fix env variable name typo

--- src/common/utils/analytics.ts
+++ src/common/utils/analytics.ts
@@ -4,7 +4,7 @@ import { SearchType } from 'common/types';
 import { AuthorityQuery, InquestQuery } from 'search/utils/api';
 
 export const initializeAnalytics = () => {
-  if (process.env.ENABLE_ANALYTICS === 'true') {
+  if (process.env.REACT_APP_ENABLE_ANALYTICS === 'true') {
     mixpanel.init('3b7e098654dd98402efe90a1c16ecda5');
   }
 };
